5c0961499c2f23fcf40c27109744446eee5ffedf
Abdella Shukur <abdella.shukur2@gmail.com>
2020-02-20 21:28:47 -0500

    exercise 7

--- ex7/script.js
+++ ex7/script.js
@@ -1,3 +1,26 @@
 // Make the section disappear and reappear whenever you click the section-header
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
+
+
+var homepage = document.getElementById("accordion");
+
+
+
+function showDiv() {
+    homepage.style.display = "block";
+}
+
+function closeDiv() {
+    homepage.style.display = "none";
+}
+
+
+homepage.addEventListener("click", function() {
+    if (homepage.style.display === "block") {
+        closeDiv()
+    } else {
+        showDiv()
+    }
+})
+
